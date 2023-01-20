b845221604dbad48b7f0696854fabff720fdf1ce
Bruce Clark <bruce.clark55@gmail.com>
2023-01-19 21:02:06 -0600

    git lit

---
+++ components/AuthorCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteAuthorBooks } from '../api/mergedData';
+
+export default function AuthorCard({ authorObj, onUpdate }) {
+  const deleteThisAuthor = () => {
+    if (window.confirm(`Delete ${authorObj.first_name}${authorObj.last_name}?`)) {
+     deleteAuthorBooks(authorObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={authorObj.image} alt={authorObj.image} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>{authorObj.first_name}</Card.Title>
+        <Card.Title>{authorObj.last_name}</Card.Title>
+        {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
+        <Link href={`/book/${authorObj.firebaseKey}`} passHref>
+            <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+        <Link href={`/book/edit/${authorObj.firebaseKey}`} passHref>
+            <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisAuthor} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+    );
+}
+
+AuthorCard.propTypes = {
+  authorObj: PropTypes.shape({
+    email: PropTypes.string,
+    firebaseKey: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    image: PropTypes.string,
+    favorite: PropTypes.bool,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
